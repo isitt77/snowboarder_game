adfaed79d136fb4328e63178a49eedd73bb03768
Scott Isitt <isitts@hotmail.com>
2022-03-11 00:50:51 -0800

    Add level reload delay with Invoke method to CrashDetector class.

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,12 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float crashReloadDelay = 1f;
+
    void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
             Debug.Log("You crashed!");
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", crashReloadDelay);
         }   
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
