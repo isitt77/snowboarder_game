fa550cd55015c4bc54e87e429a55a7a25becec38
Scott Isitt <isitts@hotmail.com>
2022-03-11 00:39:22 -0800

    Add Invoke method to delay reload of scene in FinishLine.cs.

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,12 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 1f;
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Player")
         {
             Debug.Log("You crossed Finish Line!"); 
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", reloadDelay);
         }   
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
