f33921a218654bb8ef123013e45cd5009e9c2591
Scott Isitt <isitts@hotmail.com>
2022-03-11 23:24:48 -0800

    Add Finish Effect particle system object to FinishLine class to trigger effect.

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,12 +6,14 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float reloadDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
 
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Player")
         {
             Debug.Log("You crossed Finish Line!"); 
+            finishEffect.Play();
             Invoke("ReloadScene", reloadDelay);
         }   
     }
